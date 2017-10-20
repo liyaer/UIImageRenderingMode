5540c8361faabe300bf7766a006e9c826ba5eeaf
DuWenLiang <739285514@qq.com>
2017-10-20 15:12:08 +0800

    Initial Commit

---
+++ UIImageRenderingMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageRenderingMode
+//
+//  Created by 杜文亮 on 2017/10/20.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageRenderingMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageRenderingMode
+//
+//  Created by 杜文亮 on 2017/10/20.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
